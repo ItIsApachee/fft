4ac1a2cb5fe1bfed7fc0cddb70638b828f1a3d1e
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-12-07 17:13:15 +0300

    Add dft implementation using chirp-z

---
+++ include/apachee/fft/dft.hpp
@@ -0,0 +1,23 @@
+#include <complex>
+#include <cmath>
+#include <vector>   
+#include <numbers>
+#include <stdint.h>
+
+#include <apachee/fft/chirp_z.hpp>
+
+namespace apachee::fft {
+
+
+
+template<typename RandomIt>
+std::vector<std::complex<std::double_t>> discrete_fourier_transform(RandomIt first, RandomIt last) {
+    int64_t sequence_size = last - first;
+    std::double_t angle = - 2 * std::numbers::pi_v<std::double_t> / static_cast<std::double_t>(sequence_size);
+    std::complex<std::double_t> C = std::exp(std::complex<std::double_t>{0, angle});
+    return chirp_z(first, last, C);
+}
+
+
+
+} // namespace apachee::fft
