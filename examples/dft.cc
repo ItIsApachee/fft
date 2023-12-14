40380cac504bf9992c1a3162620b3007c6573f03
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-12-14 05:38:07 +0300

    Add example for DFT

---
+++ examples/dft.cc
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <bit>
+#include <stdint.h>
+
+#include <apachee/fft/fft.hpp>
+#include "utils.hpp"
+
+int main() {
+    auto poly = polynomial_prompt("P");
+
+    print_poly(poly, "P");
+
+    std::cout << "DFT..." << std::endl;
+    auto result = apachee::fft::discrete_fourier_transform(poly.begin(), poly.end());
+    
+    std::cout << "DFT: ";
+    for (const auto &i : result) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
