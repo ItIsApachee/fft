f8ffcfac4f32cbdf69bc59bb3cda1aca36cfec11
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-12-14 04:18:06 +0300

    Add complex convolution

--- include/apachee/fft/convolution.h
+++ include/apachee/fft/convolution.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <complex>
 
 namespace apachee::fft {
 
@@ -8,6 +9,11 @@ namespace apachee::fft {
 std::vector<std::double_t>
 convolution(
         const std::vector<std::double_t>& a, const std::vector<std::double_t>& b,
+        std::double_t eps = 1E-12);
+
+std::vector<std::complex<std::double_t>>
+complex_convolution(
+        const std::vector<std::complex<std::double_t>>& a, const std::vector<std::complex<std::double_t>>& b,
         std::double_t eps = 1E-12);
 
 
--- src/convolution.cc
+++ src/convolution.cc
@@ -50,6 +50,40 @@ convolution(
     return result;
 }
 
+std::vector<std::complex<std::double_t>>
+complex_convolution(
+        const std::vector<std::complex<std::double_t>>& a_, const std::vector<std::complex<std::double_t>>& b_,
+        std::double_t eps) {
+    if (a_.empty() || b_.empty())
+        return {};
+
+    using complex_t = std::complex<std::double_t>;
+
+    int64_t result_size = std::bit_ceil(a_.size() + b_.size() - 1);
+
+    std::vector<complex_t> complex_result(a_.size());
+    for (int64_t i = 0; i < a_.size(); i++)
+        complex_result[i] = a_[i];
+
+    complex_result.resize(result_size, 0);
+
+    std::vector<complex_t> b(b_.size());
+    for (int64_t i = 0; i < b_.size(); i++)
+        b[i] = b_[i];
+
+    internal::fft(complex_result.begin(), complex_result.end());
+    internal::fft(b.begin(), b.end());
+
+    for (int64_t i = 0; i < result_size; i++)
+        complex_result[i] *= b[i];
+
+    internal::fft(complex_result.begin(), complex_result.end(), true);
+    while (complex_result.size() > 1 && abs(complex_result.back()) <= eps)
+        complex_result.pop_back();
+
+    return complex_result;
+}
+
 
 
 }  // namespace apachee::fft
