67181714a4f2ab501f76406a637f28b4435c4b51
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-12-14 05:37:56 +0300

    Add degree prompt

--- examples/czt.cc
+++ examples/czt.cc
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <numbers>
+#include <cassert>
 
 #include <apachee/fft/fft.hpp>
 #include "utils.hpp"
@@ -14,7 +15,15 @@ int main() {
     auto pi = std::numbers::pi_v<std::double_t>;
     std::complex<std::double_t> C = std::exp(std::complex<std::double_t>{0, pi * phi});
 
-    auto result = apachee::fft::chirp_z_transform(poly.begin(), poly.end(), C);
+    int64_t degree = -1;
+    std::cout << "enter degree for CZT (it will evaluate at points C^0, ..., C^(degree - 1)) or put -1 to use polynomial degree: ";
+    std::cin >> degree;
+
+    // invalid degree
+    assert(!(degree < 0 && degree != -1));
+    
+
+    auto result = apachee::fft::chirp_z_transform(poly.begin(), poly.end(), C, degree);
 
     for (int64_t i = 0; i < result.size(); i++) {
         std::cout << "P(" << std::pow(C, i) << ") = " << result[i] << std::endl;
