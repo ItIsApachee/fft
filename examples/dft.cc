2fffa333f45d6153a447e8c0114694b4c301a7ea
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-12-18 00:56:04 +0300

    Add IDFT to example

--- examples/dft.cc
+++ examples/dft.cc
@@ -11,11 +11,15 @@ int main() {
     print_poly(poly, "P");
 
     std::cout << "DFT..." << std::endl;
-    auto result = apachee::fft::discrete_fourier_transform(poly.begin(), poly.end());
+    auto result = apachee::fft::dft(poly.begin(), poly.end());
     
     std::cout << "DFT: ";
     for (const auto &i : result) {
         std::cout << i << " ";
     }
+    std::cout << std::endl;
+
+    poly = apachee::fft::idft(result.begin(), result.end());
+    print_poly(poly, "IDFT(DFT(P))");
     std::cout << std::endl;
 }
